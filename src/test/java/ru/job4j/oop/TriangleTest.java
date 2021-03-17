798a87178b227e62878d2390fc9854919f028359
kva.devops <kva.devops@gmail.com>
2021-03-17 10:02:42 +0400

    2.2. Рефакторинг - Площадь треугольника. [#485034]

---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.oop;
+
+import junit.framework.TestCase;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class TriangleTest extends TestCase {
+
+    public void testPeriod() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a , b, c);
+        double rsl = triangle.period(a.distance(b), a.distance(c), b.distance(c));
+        assertThat(rsl, closeTo(6.828, 0.001));
+    }
+
+    public void testExist() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        double ab = a.distance(b);
+        double ac = a.distance(c);
+        double bc = b.distance(c);
+        Triangle triangle = new Triangle(a , b, c);
+        boolean rsl = triangle.exist(ab, ac, bc);
+        assertThat(rsl, is(true));
+    }
+
+    public void testArea() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(8, 0.001));
+    }
+}
