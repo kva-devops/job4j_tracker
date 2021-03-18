feaee0468bf5c5fe8f3aca9aa7463a9d1db3928b
kva.devops <kva.devops@gmail.com>
2021-03-18 09:34:46 +0400

    2. Перегрузка конструктора [#485049]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,7 +2,20 @@ package ru.job4j.tracker;
 
 public class Item {
     private int id;
+
     private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 
     public int getId() {
         return id;
@@ -18,5 +31,11 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item ob1 = new Item();
+        Item ob2 = new Item("Something");
+        Item ob3 = new Item("Else", 1233);
     }
 }
