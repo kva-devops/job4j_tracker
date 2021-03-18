faafd404c371dd2cfa04d5ec6e295927f3936b7f
kva.devops <kva.devops@gmail.com>
2021-03-18 14:07:26 +0400

    7. Date. Отображение даты. [#485053]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,18 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
 
     private String name;
+
+    private LocalDateTime created = LocalDateTime.now();
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
 
     public Item() {
     }
@@ -31,11 +40,5 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public static void main(String[] args) {
-        Item ob1 = new Item();
-        Item ob2 = new Item("Something");
-        Item ob3 = new Item("Else", 1233);
     }
 }
