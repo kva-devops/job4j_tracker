790ab5ef3fec38c335b126e2f5d78c771e9295d6
kva.devops <kva.devops@gmail.com>
2021-03-19 18:54:58 +0400

    7. Метод удаления заявки Tracker.delete [#485062]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -54,4 +54,15 @@ public class Tracker {
         items[index].setId(id);
         return true;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        }
+        System.arraycopy(items, index + 1, items, index, size - index);
+        items[size - 1] = null;
+        size--;
+        return true;
+    }
 }
