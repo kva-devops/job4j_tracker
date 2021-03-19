ebbd0ee86f428d0ff23e1095e5a6895c855b8d15
kva.devops <kva.devops@gmail.com>
2021-03-19 18:38:10 +0400

    6. Метод замены заявки. Tracker.replace [#485061]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -29,15 +29,29 @@ public class Tracker {
         return Arrays.copyOf(result, counter);
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        }
+        items[index] = item;
+        items[index].setId(id);
+        return true;
     }
 }
