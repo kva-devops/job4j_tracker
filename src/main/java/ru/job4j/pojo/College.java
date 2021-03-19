bad8d5c3d7033d1cac15340564550fa0dc45f686
kva.devops <kva.devops@gmail.com>
2021-03-19 08:56:36 +0400

    2. Модель данных. [#485057]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student person1 = new Student();
+        person1.setName("Ivanov Ivan Ivanovich");
+        person1.setGroup("1233441");
+        person1.setCredited(new Date());
+        System.out.println( "Student: " + person1.getName() + "\n" +
+                            "Group No: " + person1.getGroup() + "\n" +
+                            "Date of credited: " + person1.getCredited() + "\n");
+    }
+}
