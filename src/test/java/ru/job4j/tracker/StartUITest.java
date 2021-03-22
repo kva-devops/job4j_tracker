64ee0561c63dabbcfd6b14ac518326d3a3abfa97
kva.devops <kva.devops@gmail.com>
2021-03-22 15:30:13 +0400

    5.1. Тестирование. Подготовка данных. [#484995]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.CoreMatchers.*;
 
 public class StartUITest {
 
@@ -15,5 +16,30 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expect = new Item("Fix PS4");
         Assert.assertThat(created.getName(), is(expect.getName()));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "replaced item"};
+        Input input = new StubInput(answers);
+        StartUI.replaceItem(input, tracker);
+        Item replaced = tracker.findById(item.getId());
+        Assert.assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId())};
+        Input input = new StubInput(answers);
+        StartUI.deleteItem(input, tracker);
+        Assert.assertThat(
+                tracker.findById(item.getId()),
+                is(nullValue()));
     }
 }
