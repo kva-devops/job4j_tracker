0aa443cbba94ac4ac1aa21043ebee867a6d51061
kva.devops <kva.devops@gmail.com>
2021-03-24 15:46:56 +0400

    0.3. Тестирование исключений с junit [#485007]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.ex;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void testCalc() {
+        Fact.calc(-4);
+    }
+}
