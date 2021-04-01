8eed300e6c70d6b7d2c8563cc1a95fded4629b35
kva.devops <kva.devops@gmail.com>
2021-04-01 17:31:49 +0400

    1. Уникальные задачи. [#484978]

---
+++ src/test/java/ru/job4j/collection/FullSearchTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class FullSearchTest {
+
+    @Test
+    public void testExtractNumber() {
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                new Task("2", "Second desc"),
+                new Task("1", "First desc")
+        );
+        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
+        assertThat(FullSearch.extractNumber(tasks), is(expected));
+
+    }
+}
