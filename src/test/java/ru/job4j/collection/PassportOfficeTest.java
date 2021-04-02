55357e542fc111bc323d9c4965ebd688c95a10c2
kva.devops <kva.devops@gmail.com>
2021-04-02 14:10:32 +0400

    1. Паспорт и жители [#484974]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void testAdd() {
+        Citizen citizen = new Citizen("3234", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        Assert.assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+}
