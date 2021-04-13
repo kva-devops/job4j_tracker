b5721421f0a34eab1e71f67edf901e1a612538b0
kva.devops <kva.devops@gmail.com>
2021-04-13 19:10:12 +0400

    2. Отсортировать департаменты [#484955]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,10 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String[] o1Arr = o1.split("/");
         String[] o2Arr = o2.split("/");
         int result = o2Arr[0].compareTo(o1Arr[0]);
-        if (result == 0) {
-            return o1.compareTo(o2);
-        } else {
-            return result;
-        }
+        return (result == 0) ? o1.compareTo(o2) : result;
     }
 }
