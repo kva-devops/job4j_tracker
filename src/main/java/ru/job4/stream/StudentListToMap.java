79bc2426388df7ad78951f42e0be0ba86a2695bb
kva.devops <kva.devops@gmail.com>
2021-04-16 16:19:44 +0400

    4. Преобразование List в Map. [#484801]

---
+++ src/main/java/ru/job4/stream/StudentListToMap.java
@@ -0,0 +1,18 @@
+package ru.job4.stream;
+
+import ru.job4j.stream.Student;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StudentListToMap {
+    public static Map<String, Student> convert(List<Student> list) {
+        return list.stream()
+                .distinct()
+                .collect(Collectors.toMap(
+                        Student::getSurname,
+                        value -> value
+                ));
+    }
+}
---
+++ src/test/java/ru/job4/stream/StudentListToMapTest.java
@@ -0,0 +1,31 @@
+package ru.job4.stream;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import ru.job4j.stream.Student;
+import static org.hamcrest.core.Is.is;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StudentListToMapTest {
+
+    @Test
+    public void testConvert() {
+        List<Student> list = Arrays.asList(
+                new Student(10, "Ivanov"),
+                new Student(30, "Petrov"),
+                new Student(50, "Sidorov"),
+                new Student(10, "Ivanov")
+        );
+        Map<String, Student> expect = new HashMap<>();
+        expect.put("Ivanov", new Student(10, "Ivanov"));
+        expect.put("Petrov", new Student(30, "Petrov"));
+        expect.put("Sidorov", new Student(50, "Sidorov"));
+
+        Map<String, Student> result = StudentListToMap.convert(list);
+        Assert.assertThat(result, is(expect));
+    }
+}
