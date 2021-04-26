b24ea6da3ff183418eaa16ea53d629bcfae828b0
kva.devops <kva.devops@gmail.com>
2021-04-26 12:40:14 +0400

    1. Экзамен. Lambda, Stream API, JDK Features. [#484816] Дополнительная задача - correcting

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -6,53 +6,40 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EasyStream {
+    private static EasyStream eStream;
 
-    private List<Integer> buffer;
+    private final List<Integer> buffer;
 
-    static class Builder {
-        private List<Integer> buffer;
-
-        Builder buildBuffer(List<Integer> buffer) {
-            this.buffer = buffer;
-            return this;
-        }
-
-        EasyStream build() {
-            EasyStream eStream = new EasyStream(this);
-            eStream.buffer = buffer;
-            return eStream;
-        }
-    }
-
-    private EasyStream(Builder builder) {
-        buffer = builder.buffer;
+    private EasyStream(List<Integer> buffer) {
+        this.buffer = buffer;
     }
 
     public static EasyStream of(List<Integer> source) {
-        return new Builder().buildBuffer(source).build();
+            eStream = new EasyStream(source);
+        return eStream;
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
-        List<Integer> buffBefore = new Builder().buildBuffer(buffer).build().buffer;
+        List<Integer> buffBefore = eStream.buffer;
         List<Integer> buffAfter = new ArrayList<>();
         for (Integer elem : buffBefore) {
             buffAfter.add(fun.apply(elem));
         }
-        return new Builder().buildBuffer(buffAfter).build();
+        return EasyStream.of(buffAfter);
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
-        List<Integer> buffBefore = new Builder().buildBuffer(buffer).build().buffer;
+        List<Integer> buffBefore = eStream.buffer;
         List<Integer> buffAfter = new ArrayList<>();
         for (Integer elem : buffBefore) {
             if (fun.test(elem)) {
                 buffAfter.add(elem);
             }
         }
-        return new Builder().buildBuffer(buffAfter).build();
+        return EasyStream.of(buffAfter);
     }
 
     public List<Integer> collect() {
-        return new Builder().buildBuffer(buffer).build().buffer;
+        return eStream.buffer;
     }
 }
